e028c1872d6be3e2edf81f30f1a9cc7f690e36a7
techiemanish <manishtiwari627@gmail.com>
2022-12-25 00:43:27 +0530

    Root controller modified

--- src/main/java/com/ecommerce/RootController.java
+++ src/main/java/com/ecommerce/RootController.java
@@ -1,12 +1,14 @@
 package com.ecommerce;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
-    @GetMapping
-    public String rootController(){
-        return "Ecommerce Application is up and running on v1.5";
+    @GetMapping("/")
+    public ResponseEntity<?> rootController(){
+        return ResponseEntity.status(HttpStatus.OK).body("Ecommerce Application is up and running on v1.6");
     }
 }
