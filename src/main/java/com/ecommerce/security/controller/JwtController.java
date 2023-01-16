26dc5d878d1d99207de3667799e0f2a64a4ac5e4
techiemanish <manishtiwari627@gmail.com>
2023-01-17 00:48:11 +0530

    CORS enable for Token

--- src/main/java/com/ecommerce/security/controller/JwtController.java
+++ src/main/java/com/ecommerce/security/controller/JwtController.java
@@ -12,11 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "https://trueclix.netlify.app/"})
 public class JwtController {
 
     @Autowired
