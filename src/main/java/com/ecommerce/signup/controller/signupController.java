5604eb3e96fd7983e701632bc74899512f3de708
techiemanish <manishtiwari627@gmail.com>
2023-07-21 00:43:23 +0530

    Existig user exist validation in Signup API

--- src/main/java/com/ecommerce/signup/controller/signupController.java
+++ src/main/java/com/ecommerce/signup/controller/signupController.java
@@ -1,7 +1,9 @@
 package com.ecommerce.signup.controller;
 
 import com.ecommerce.signup.model.Signup;
+import com.ecommerce.utils.Error;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +18,17 @@ import com.ecommerce.signup.service.signupService;
 
     @PostMapping("/api/signup")
     public ResponseEntity<?> signup(@RequestBody Signup signup){
-        Signup save = this.signupService.save(signup);
-        return ResponseEntity.ok(save);
+        String email = signup.getEmail();
+        Signup data = this.signupService.findByEmail(email);
+        if(data != null){
+            Error error = new Error();
+            error.setStatus("User Already Exist!");
+            error.setMessage("An existing user was found with the provided credentials. Please log in or use different registration information.");
+            return ResponseEntity.status(HttpStatus.OK).body(error);
+        }
+        else{
+            Signup save = this.signupService.save(signup);
+            return ResponseEntity.ok(save);
+        }
     }
 }
